c952e6c634b6eae0f86c7c051da30ac062a6c224
Miguel Santiago <miguelsantiago1812@gmail.com>
2021-11-01 14:50:54 -0300

    Validações pelo front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 
@@ -50,7 +51,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatButtonModule,
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from '../categoria.model';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria-create',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaCreateComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria ={
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  create(): void{
+    this.service.create(this.categoria).subscribe((resposta) => {
+     this.service.mensagem("Categoria criada com sucesso!");
+     this.router.navigate(['categorias']);
+    }, err => {
+      for(let i= 0; i < err.error.errors.length; i++){
+        this.service.mensagem(err.error.errors[i].message)
+      }
+    })
+  }
 }
